523d92b69b18d4938d019934298b0e04ec214852
shucheng <shucheng@bd-apaas.com>
2023-04-10 23:59:53 +0800

    v1.8.3 add loong64

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -8,16 +8,16 @@ REPO="github.com/drone-runners/drone-runner-docker"
 
 # compile the server using the cgo
 export GOARCH=amd64
-go build -ldflags "-extldflags \"-s -w -static\"" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
+go build -ldflags "-s -w" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
 
 export GOARCH=arm64
-go build -ldflags "-extldflags \"-s -w -static\"" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
+go build -ldflags "-s -w" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
 
 export GOARCH=ppc64le
-go build -ldflags "-extldflags \"-s -w -static\"" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
+go build -ldflags "-s -w" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
 
 export GOARCH=mips64le
-go build -ldflags "-extldflags \"-s -w -static\"" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
+go build -ldflags "-s -w" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
 
 export GOARCH=loong64
-go build -ldflags "-extldflags \"-s -w -static\"" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
+go build -ldflags "-s -w" -o release/linux/${GOARCH}/drone-runner-docker ${REPO}/
